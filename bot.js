80afc9ca0d8fa62c1aa2b0b24bace2e5a31d0a08
Flodersdsdsdsdsd <43557824+Flodersdsdsdsdsd@users.noreply.github.com>
2018-09-25 05:19:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,78 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('======================================')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=======================================')
+});
+
+
+
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+
+client.on('message', message => {
+    if(message.content === '^^ريب'){
+        message.channel.send('#rep <@286088294234718209>')
+    }
+});
+  if(message.content == "<@" + `${client.user.id}` + ">"){
+client.on('message', message => {
+    if(message.content === "<@" + `${client.user.id}` + ">" + "تحويل"){
+        message.channel.send('#credit <@286088294234718209>')
+    }
+});
+
+client.on('message', message => {
+if (message.content === "<@" + `${client.user.id}` + ">" + "سبام") {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`احبك انا ترا , سبام يولد ينعل ام الفلة hello Again ${x}`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+const prefix = '-'
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+
+
+client.login(process.env.TOKEN);
